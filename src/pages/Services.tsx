42e048463b3979913f1e2798e4fd0a7e5bd173a9
ahmedmahmoudallam8-cyber <ahmedmahmoudallam8@gmail.com>
2025-11-06 09:50:29 +0200

    Updated Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -83,9 +83,12 @@ function Services() {
                       Learn More
                     </Link>
                   ) : (
-                    <button className="bg-emerald-600 hover:bg-emerald-700 text-white font-semibold px-6 py-3 rounded-lg transition-all duration-300 w-fit transform hover:scale-110 active:scale-95">
+                    <Link
+                      to="/services/branding"
+                      className="bg-emerald-600 hover:bg-emerald-700 text-white font-semibold px-6 py-3 rounded-lg transition-all duration-300 w-fit block transform hover:scale-110 active:scale-95 text-center"
+                    >
                       Learn More
-                    </button>
+                    </Link>
                   )}
                 </div>
               );
